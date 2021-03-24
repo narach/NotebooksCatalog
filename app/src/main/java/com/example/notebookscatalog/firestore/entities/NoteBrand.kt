ab43e263d51c6baec393d996d4dc440292bb0f91
Siarhei Naralenkau <siarhei.naralenkau@godel.shellenergy.co.uk>
2021-03-24 09:23:11 +0300

    Adding Firebase Firestore

---
+++ app/src/main/java/com/example/notebookscatalog/firestore/entities/NoteBrand.kt
@@ -0,0 +1,5 @@
+package com.example.notebookscatalog.firestore.entities
+
+data class NoteBrand(
+    var name: String
+)
--- app/src/main/java/com/example/notebookscatalog/ui/MainActivity.kt
+++ app/src/main/java/com/example/notebookscatalog/ui/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.notebookscatalog.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.notebookscatalog.NotebooksApplication
 import com.example.notebookscatalog.R
 import com.example.notebookscatalog.databinding.ActivityMainBinding
+import com.example.notebookscatalog.firestore.entities.NoteBrand
 import com.example.notebookscatalog.interfaces.IFragmentCommunication
 import com.example.notebookscatalog.ui.fragments.AddFragment
 import com.example.notebookscatalog.ui.fragments.EditFragment
@@ -15,6 +17,13 @@ import com.example.notebookscatalog.viewmodels.BrandViewModel
 import com.example.notebookscatalog.viewmodels.BrandViewModelFactory
 import com.example.notebookscatalog.viewmodels.DeviceViewModel
 import com.example.notebookscatalog.viewmodels.DeviceViewModelFactory
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity(), IFragmentCommunication  {
 
@@ -22,6 +31,8 @@ class MainActivity : AppCompatActivity(), IFragmentCommunication  {
     private lateinit var fDevicesList: ListFragment
     private lateinit var fDeviceEdit: EditFragment
     private lateinit var fDeviceAdd: AddFragment
+
+    private val brandsCollectionRef = Firebase.firestore.collection("brands")
 
     private val brandViewModel: BrandViewModel by viewModels {
         BrandViewModelFactory((application as NotebooksApplication).brandRepository)
@@ -31,11 +42,30 @@ class MainActivity : AppCompatActivity(), IFragmentCommunication  {
         DeviceViewModelFactory((application as NotebooksApplication).deviceRepository)
     }
 
+    // Save new brand in DB
+    private fun saveBrand(brand: NoteBrand) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                brandsCollectionRef.add(brand).await()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "Successfully saved data.", Toast.LENGTH_LONG).show()
+                }
+            } catch(e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val rootView = binding.root
         setContentView(rootView)
+
+        val newBrand = NoteBrand("HP")
+        saveBrand(newBrand)
 
         fDevicesList = ListFragment(this, deviceViewModel)
         fDeviceEdit = EditFragment(this, deviceViewModel)
